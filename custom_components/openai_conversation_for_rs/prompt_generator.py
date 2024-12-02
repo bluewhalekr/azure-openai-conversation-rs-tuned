e3887f26e272c53548287c269ce9e9af23f5f410
Lee MyoungHwan <amaramusic@gmail.com>
2024-12-03 07:28:46 +0900

    fix: make chat method in GptHaAssistant asynchronous

--- custom_components/openai_conversation_for_rs/prompt_generator.py
+++ custom_components/openai_conversation_for_rs/prompt_generator.py
@@ -120,7 +120,7 @@ class GptHaAssistant:
         """Chat with the GPT-based Home Assistant"""
         self.model_input_messages = self.add_instructions(chat_history)
 
-        response = self.openai_client.chat.completions.create(
+        response = await self.openai_client.chat.completions.create(
             model=self.deployment_name, messages=self.model_input_messages, tools=self.tool_prompts, n=n
         )
 
